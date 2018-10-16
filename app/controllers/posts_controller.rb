a8dfe323ac4d4b0d35d9418763c8c277b328fd7a
Katherine Worley <kdesign216@yahoo.com>
2018-10-16 08:07:25 -0400

    adding post controlers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
 	def index
 	end
+
+	def show
+	end
+
+	def new 
+	end 
+
+	def create 
+	end
+
+	def update
+	end 
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_post
+	end
+
+	def post_params
+	end
+
+
 end
