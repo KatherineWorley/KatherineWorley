a41e4438166e97b2dff6ce6c6b62e6a37bb2ea18
Katherine Worley <kdesign216@yahoo.com>
2018-10-23 10:55:29 -0400

    add application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,9 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-	protected 
+  protected
 
-	def configure_permitted_parameters
-			devise_parameter_sanitizer.for(:sign_up) << :name
-			devise_parameter_sanitizer.for(:account_update) << :name
-	end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
