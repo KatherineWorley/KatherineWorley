e3c09bfa7e91b64503e16a2eee1e941936eb3c77
Katherine Worley <kdesign216@yahoo.com>
2018-11-12 09:00:02 -0500

    adding images to posts file and view page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
 	end
 
 	def post_params
-		params.require(:post).permit(:title, :description, :link)
+		params.require(:post).permit(:title, :description, :link, :image)
 	end
 end
