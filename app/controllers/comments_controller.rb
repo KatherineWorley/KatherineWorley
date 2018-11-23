c7e6e028fd9baa68f893b47f45501804a302aac8
Katherine Worley <kdesign216@yahoo.com>
2018-11-23 07:40:36 -0500

    working on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
+	
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = Comment.create(params[:comment].permit(:content))
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many_attached :photo
 	# has_many_attached :photo, style: { medium: "700x500", small: "350x250" }
-	has_many :comments
-	
+	has_many :comments	
 end
